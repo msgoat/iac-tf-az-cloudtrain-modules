d60abc25b923676a0370a118a7eea5db075a6817
Michael Theis (msg) <michael.theis@msg.group>
2024-02-08 18:56:31 +0100

    container/addons: adapted to changes in container/addon/azure-storage-classes

--- modules/container/aks/addons/main.tf
+++ modules/container/aks/addons/main.tf
@@ -1,28 +1,27 @@
-module azure_storage_classes {
-  source = "../addon/azure-storage-classes"
-  count = var.addon_azure_storage_classes_enabled ? 1 : 0
-  region_name = var.region_name
-  region_code = var.region_code
-  solution_name = var.solution_name
-  solution_stage = var.solution_stage
-  solution_fqn = var.solution_fqn
-  common_tags = var.common_tags
+module "azure_storage_classes" {
+  source            = "../addon/azure-storage-classes"
+  count             = var.addon_azure_storage_classes_enabled ? 1 : 0
+  region_name       = var.region_name
+  region_code       = var.region_code
+  solution_name     = var.solution_name
+  solution_stage    = var.solution_stage
+  solution_fqn      = var.solution_fqn
+  common_tags       = var.common_tags
   resource_group_id = var.resource_group_id
-  aks_cluster_id = var.aks_cluster_id
-  key_vault_id = var.key_vault_id
+  aks_cluster_id    = var.aks_cluster_id
 }
 
-module cert_manager {
-  source = "../addon/cert-manager"
-  count = var.addon_cert_manager_enabled ? 1 : 0
-  region_name = var.region_name
-  region_code = var.region_code
-  solution_name = var.solution_name
-  solution_stage = var.solution_stage
-  solution_fqn = var.solution_fqn
-  common_tags = var.common_tags
-  aks_cluster_id = var.aks_cluster_id
-  resource_group_id = var.resource_group_id
-  dns_zone_id = var.dns_zone_id
+module "cert_manager" {
+  source                   = "../addon/cert-manager"
+  count                    = var.addon_cert_manager_enabled ? 1 : 0
+  region_name              = var.region_name
+  region_code              = var.region_code
+  solution_name            = var.solution_name
+  solution_stage           = var.solution_stage
+  solution_fqn             = var.solution_fqn
+  common_tags              = var.common_tags
+  aks_cluster_id           = var.aks_cluster_id
+  resource_group_id        = var.resource_group_id
+  dns_zone_id              = var.dns_zone_id
   letsencrypt_account_name = var.letsencrypt_account_name
 }
--- modules/container/aks/addons/variables.tf
+++ modules/container/aks/addons/variables.tf
@@ -1,72 +1,72 @@
-variable region_name {
+variable "region_name" {
   description = "The Azure region to deploy into."
-  type = string
+  type        = string
 }
 
-variable region_code {
+variable "region_code" {
   description = "The code of Azure region to deploy into (supposed to be a meaningful abbreviation of region_name)."
-  type = string
+  type        = string
 }
 
-variable common_tags {
+variable "common_tags" {
   description = "Map of common tags to be attached to all managed Azure resources"
-  type = map(string)
+  type        = map(string)
 }
 
-variable solution_name {
+variable "solution_name" {
   description = "Name of this Azure solution"
-  type = string
+  type        = string
 }
 
-variable solution_stage {
+variable "solution_stage" {
   description = "Stage of this Azure solution"
-  type = string
+  type        = string
 }
 
-variable solution_fqn {
+variable "solution_fqn" {
   description = "Fully qualified name of this Azure solution"
-  type = string
+  type        = string
 }
 
-variable resource_group_id {
+variable "resource_group_id" {
   description = "Unique identifier of the resource group supposed to own all allocated resources"
-  type = string
+  type        = string
 }
 
-variable aks_cluster_id {
+variable "aks_cluster_id" {
   description = "Unique identifier of the target AKS cluster"
-  type = string
+  type        = string
 }
 
-variable key_vault_id {
+variable "key_vault_id" {
   description = "Unique identifier of the Key Vault managing all confidential data of this solution"
-  type = string
+  type        = string
 }
 
-variable addon_cert_manager_enabled {
+variable "addon_cert_manager_enabled" {
   description = "controls if the Kubernetes cert-manager is installed on the given AKS cluster"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
-variable addon_azure_storage_classes_enabled {
+variable "addon_azure_storage_classes_enabled" {
   description = "controls if the additional Azure specific storage classes are installed on the given AKS cluster"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
-variable node_group_workload_class {
+variable "node_group_workload_class" {
   description = "Class of the AKS node group this tool stack should be hosted on"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "dns_zone_id" {
   description = "Unique identifier of a public DNS supposed contain all public DNS records to route traffic to the Kubernetes cluster"
-  type = string
+  type        = string
 }
 
 variable "letsencrypt_account_name" {
   description = "Lets Encrypt Account name to be used to request certificates"
-  type = string
+  type        = string
 }
